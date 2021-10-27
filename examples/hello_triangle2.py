1f8bf702d8e600098321f50a8f62d759e3740262
Christopher Bruns <cbruns@esri.com>
2021-10-27 16:18:16 -0700

    Add comments

--- examples/hello_triangle2.py
+++ examples/hello_triangle2.py
@@ -1,9 +1,13 @@
 """
+Test modular window type
 """
 
+# Pluggable renderers
 from volview.actor.hello_triangle import HelloTriangle
+
+# Pluggable window systems
 from volview.window.glfw_window import GlfwWindow
 from volview.window.qt_window import SimpleQtWindow
 
-with GlfwWindow(renderer=HelloTriangle()) as window:
+with SimpleQtWindow(renderer=HelloTriangle()) as window:
     window.render_loop()
